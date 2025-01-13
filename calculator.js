1a4b399ebf0a73fca8e8a202430220cdebf55210
tarasowski <dimitri+github@tarasowski.de>
2025-01-13 09:24:30 +0100

    feat: added a multiply function

--- calculator.js
+++ calculator.js
@@ -2,3 +2,7 @@ export function add(a, b) {
   return a + b
 }
 
+export function multiply(a, b) {
+  return a * b
+}
+
--- calculator.test.js
+++ calculator.test.js
@@ -1,10 +1,19 @@
 import assert from "assert"
-import { add } from "./calculator.js"
+import { add, multiply } from "./calculator.js"
 
 
+{
+  const want = 4
+  const got = add(2,2)
 
-const want = 4
-const got = add(2,2)
+  assert.equal(got, want)
+}
 
-assert.equal(got, want)
+{
+  const want = 8
+  const got = multiply(4, 2) 
+
+  assert.equal(got, want)
+}
+
 
